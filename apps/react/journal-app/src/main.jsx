9a47c345989b410e47fbe0192bea6a11b20409a1
Ale Roses <ale.vrs@outlook.com>
2025-06-13 14:46:15 -0500

    feat(apps/react/journal-app): JournalApp initial structure with routing and authentication
    Added custom routing and modular structure (auth, journal, router)

--- apps/react/journal-app/src/main.jsx
+++ apps/react/journal-app/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router";
+
+import JournalApp from "./JournalApp.jsx";
+import "./styles.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
+  </StrictMode>
+);
