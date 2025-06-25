06f6449e2380b79dbd546a7dfe69be9bd3810d4d
Ale Roses <ale.vrs@outlook.com>
2025-06-25 14:40:30 -0500

    chore(apps/react/journal-app): Add Material UI dependecies

--- apps/react/journal-app/src/main.jsx
+++ apps/react/journal-app/src/main.jsx
@@ -6,6 +6,11 @@ import { BrowserRouter } from "react-router";
 import JournalApp from "./JournalApp.jsx";
 import "./styles.css";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
