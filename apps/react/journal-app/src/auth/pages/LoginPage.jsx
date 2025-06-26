9d48bcd56cb33dbe36d39c9e4e2f691fae21a0eb
Ale Roses <ale.vrs@outlook.com>
2025-06-26 17:26:49 -0500

    feat(apps/react/journal-app): Add a Login, Google and create account buttons.

--- apps/react/journal-app/src/auth/pages/LoginPage.jsx
+++ apps/react/journal-app/src/auth/pages/LoginPage.jsx
@@ -3,7 +3,11 @@ import {
   Typography,
   TextField,
   Grid,
+  Button,
+  Link,
 } from "@mui/material";
+import { Link as RouterLink } from "react-router";
+import { Google } from "@mui/icons-material";
 
 export const LoginPage = () => {
   return (
@@ -59,6 +63,32 @@ export const LoginPage = () => {
               fullWidth
             />
           </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Button variant="contained" fullWidth>
+              Login
+            </Button>
+          </Grid>
+          <Grid size={{ xs: 12, md: 6 }}>
+            <Button
+              variant="contained"
+              fullWidth
+              startIcon={<Google />}
+            >
+              Google
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="end" sx={{ mt: 2 }}>
+          <Link
+            component={RouterLink}
+            color="inherit"
+            to="/auth/register"
+          >
+            Create an account
+          </Link>
         </Grid>
       </Box>
     </Box>
