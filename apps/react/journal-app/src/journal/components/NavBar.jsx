7e0213280f21b1939472a18978eb869a0da51314
Ale Roses <ale.vrs@outlook.com>
2025-06-28 21:39:06 -0500

    feat(apps/react/journal-app): Create NavBar

---
+++ apps/react/journal-app/src/journal/components/NavBar.jsx
@@ -0,0 +1,68 @@
+import {
+  LogoutOutlined,
+  MenuOutlined,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+} from "@mui/material";
+import { useState } from "react";
+
+export const NavBar = ({ drawerWidth = 240 }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <AppBar
+        // position="static"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            aria-label=""
+            onClick={() => setOpen(true)}
+            color="inherit"
+            edge="start"
+            sx={{
+              display: { xs: "flex", sm: "none" },
+              mr: 2,
+            }}
+          >
+            <MenuOutlined />
+          </IconButton>
+
+          <Typography
+            variant="h6"
+            component="span"
+            sx={{ flexGrow: 1 }}
+          >
+            JournalApp
+          </Typography>
+
+          <IconButton aria-label="" color="error">
+            <LogoutOutlined />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{
+          display: { xs: "flex", sm: "none" },
+        }}
+      >
+        <Typography variant="h6" color="initial">
+          LolCat
+        </Typography>
+      </Drawer>
+    </>
+  );
+};
--- apps/react/journal-app/src/journal/layout/JournalLayout.jsx
+++ apps/react/journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,13 @@
 import { Box } from "@mui/material";
+import { NavBar } from "../components/NavBar";
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       {/* Navbar draweWidth */}
+      <NavBar drawerWidth={drawerWidth} />
 
       {/* Sidebar draweWidth */}
 
