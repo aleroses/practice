db60cb402026ff71403e935fb76a44d2c8d5569b
Ale Roses <ale.vrs@outlook.com>
2025-07-01 16:54:38 -0500

    feat(apps/react/journal-app): Create floating button

--- apps/react/journal-app/src/journal/pages/JournalPage.jsx
+++ apps/react/journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,33 @@
 import { JournalLayout } from "../layout/JournalLayout";
-import { NoteView } from "../views/NoteView";
-// import { NothingSelectedView } from "../views/NothingSelectedView";
+// import { NoteView } from "../views/NoteView";
+import IconButton from "@mui/material/IconButton";
+import { NothingSelectedView } from "../views/NothingSelectedView";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        aria-label=""
+        size="large"
+        sx={{
+          color: "white",
+          bgcolor: "error.main",
+          ":hover": {
+            bgcolor: "error.main",
+            opacity: 0.9,
+          },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+          // onClick={}
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
