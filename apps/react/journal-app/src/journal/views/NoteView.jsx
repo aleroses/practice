cd378319609e472e053c71122ea2bad1c931f8e3
Ale Roses <ale.vrs@outlook.com>
2025-06-30 19:04:54 -0500

    feat(apps/react/journal-app): Create the NoteView component

---
+++ apps/react/journal-app/src/journal/views/NoteView.jsx
@@ -0,0 +1,68 @@
+import { SaveOutlined } from "@mui/icons-material";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+import { ImageGallery } from "../components/ImageGallery";
+
+export const NoteView = () => {
+  return (
+    <>
+      <Grid
+        constainer
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{
+          mb: 1,
+        }}
+      >
+        <Grid>
+          <Typography
+            variant="h5"
+            fontSize={39}
+            fontWeight="light"
+          >
+            August 28, 2023
+          </Typography>
+        </Grid>
+
+        <Grid>
+          <Button color="primary" sx={{ padding: 2 }}>
+            <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          </Button>
+        </Grid>
+
+        <Grid container sx={{ flexGrow: 1 }}>
+          <TextField
+            id=""
+            type="text"
+            variant="filled"
+            label="Title"
+            // value={}
+            // onChange={}
+            placeholder="Enter a title"
+            fullWidth
+            sx={{ border: "none", mb: 1 }}
+          />
+          <TextField
+            id=""
+            type="text"
+            variant="filled"
+            // label="Title"
+            // value={}
+            // onChange={}
+            placeholder="What happened today?"
+            fullWidth
+            multiline
+            minRows={5}
+            sx={{ border: "none", mb: 1 }}
+          />
+        </Grid>
+
+        <ImageGallery />
+      </Grid>
+    </>
+  );
+};
