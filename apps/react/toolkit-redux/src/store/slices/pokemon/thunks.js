87df7625d27519124fed18a61fc7aa5a676366ef
Ale Roses <ale.vrs@outlook.com>
2025-07-10 14:27:14 -0500

    feat(apps/react/toolkit-redux): Implement axios

--- apps/react/toolkit-redux/src/store/slices/pokemon/thunks.js
+++ apps/react/toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,7 +1,22 @@
-import { startLoadingPokemons } from "./pokeSlice";
+import { pokeApi } from "../../../api/pokeApi";
+import {
+  setPokemons,
+  startLoadingPokemons,
+} from "./pokeSlice";
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
+
+    const { data } = await pokeApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
+    );
+
+    dispatch(
+      setPokemons({
+        pokemons: data.results,
+        page: page + 1,
+      })
+    );
   };
 };
