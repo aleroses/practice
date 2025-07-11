546f8d5eb1ec00c47e52c5c59acdece2343dc9c7
Ale Roses <ale.vrs@outlook.com>
2025-07-11 17:56:56 -0500

    feat(apps/react/journal-app): Configuring AuthSlice

---
+++ apps/react/journal-app/src/store/auth/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: "checking",
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, payload) => {},
+    checkingCredentials: (state) => {},
+  },
+});
+
+export const { login, logout, checkingCredentials } =
+  authSlice.actions;
