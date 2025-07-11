9d89676483522d93f573845fef967740dcd33c09
Ale Roses <ale.vrs@outlook.com>
2025-07-10 21:51:45 -0500

    feat(apps/react/toolkit-redux): update

--- apps/react/toolkit-redux/src/PokeApp.jsx
+++ apps/react/toolkit-redux/src/PokeApp.jsx
@@ -1,13 +1,39 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon/thunks";
+import { useSelector } from "react-redux";
 
 export const PokeApp = () => {
+  const { isLoading, pokemons, page } = useSelector(
+    (state) => state.poke
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPokemons());
   }, []);
 
-  return <div>PokeApp</div>;
+  return (
+    <>
+      <h1>PokeApp</h1>
+      <hr />
+      <span>
+        Loading:{" "}
+        {isLoading ? `True :/ ${page}` : `False :) ${page}`}
+      </span>
+
+      <ul>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button
+        disabled={isLoading}
+        onClick={() => dispatch(getPokemons(page))}
+      >
+        Next
+      </button>
+    </>
+  );
 };
