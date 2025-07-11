42bd9baea5c1958066ed599c16bf6ca6db4e5721
Ale Roses <ale.vrs@outlook.com>
2025-07-11 17:03:43 -0500

    feat(apps/react/journal-app): Configure Redux

--- apps/react/journal-app/src/main.jsx
+++ apps/react/journal-app/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+
 import JournalApp from "./JournalApp.jsx";
 import "./styles.css";
 
@@ -12,8 +15,10 @@ import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
